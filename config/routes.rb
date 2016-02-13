9bc9a7fb0e61a31088ab54493811ce4707754349
dryyun <dryyun@gmail.com>
2016-02-13 15:45:37 +0800

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
+  get 'static_pages/about'
+
   resources :microposts
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
@@ -10,7 +12,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'application#hello'
+  # root 'application#hello'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
