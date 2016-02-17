4b814e691abbc35b5753b968aa720949d388663d
dryyun <dryyun@gmail.com>
2016-02-17 11:17:39 +0800

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,13 +12,13 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
-  def User.digest(string)
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 
   # 返回一个随机令牌
-  def User.new_token
+  def self.new_token
     SecureRandom.urlsafe_base64
   end
 
