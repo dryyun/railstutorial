a991a379abfc863ed21bbcdd4ca08b7ec391f587
dryyun <dryyun@gmail.com>
2016-02-17 22:51:35 +0800

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,15 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_save :create_activation_digest
 
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255},
             format: {with: VALID_EMAIL_REGEX},
             uniqueness: {case_sensitive: false}
-  validates :password, presence: true, length: {minimum: 6},allow_nil: true
+  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
 
-  before_save { self.email = email.downcase }
 
   has_secure_password
 
@@ -37,6 +38,19 @@ class User < ActiveRecord::Base
   # 忘记用户
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  private
+
+  # 把电子邮件地址转换成小写
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  # 创建并赋值激活令牌和摘要
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 
 end
