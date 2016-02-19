c45e9a1f4f8b15e14d7f28fc44c02632efe2595d
dryyun <dryyun@gmail.com>
2016-02-19 11:40:41 +0800

    commit

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,7 +4,7 @@ class MicropostTest < ActiveSupport::TestCase
   def setup
     @user = users(:michael)
     # 这行代码不符合常见做法
-    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+    @micropost = @user.microposts.build(content: "Lorem ipsum")
   end
 
   test "should be valid" do
@@ -14,5 +14,19 @@ class MicropostTest < ActiveSupport::TestCase
   test "user id should be present" do
     @micropost.user_id = nil
     assert_not @micropost.valid?
+  end
+
+  test "content should be present" do
+    @micropost.content = " "
+    assert_not @micropost.valid?
+  end
+
+  test "content should be at most 140 characters" do
+    @micropost.content = 'a' * 141
+    assert_not @micropost.valid?
+  end
+
+  test "order should be most recent first" do
+    assert_equal Micropost.first, microposts(:most_recent)
   end
 end
